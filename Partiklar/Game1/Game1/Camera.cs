7734ca6697f43fabad4cf53376f4bd3d1a707932
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-11-23 11:51:43 +0100

    Started on splitter

---
+++ Partiklar/Game1/Game1/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    class Camera
+    {
+        public void setFieldSize(Viewport port)
+        {
+
+        }
+    }
+}
